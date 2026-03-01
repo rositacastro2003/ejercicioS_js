72d5d6671ed71a3cc732aabf156d513c1ed2d0cf
Brenda <castrorosita377@gmail.com>
2026-03-01 15:09:33 -0500

    ejercicio 6 ok

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -28,7 +28,7 @@ const nuevaMultiplicacion = 10*4===40;
 
 // Test 6
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21%5===1;
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
